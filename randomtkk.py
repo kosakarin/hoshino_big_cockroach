72a6e90a4b32dfb11ffa1843656fc6913935f30b
kosakarin <91523573+kosakarin@users.noreply.github.com>
2021-12-01 10:40:49 +0800

    重写了游戏状态检查的算法
    之前那个是临时用的，因为一时脑抽把{}打成[]疯狂报错我自己还没检查出来报错原因然后急眼了就先那样将就了，反正现在发现了用上了一开始的方案，感觉清爽多了

--- randomtkk.py
+++ randomtkk.py
@@ -21,9 +21,10 @@ ex_num = 40    #地狱难度数量
 max_num = 80   #最大数量 超过默认使用ez
 waittime = 30  #num = 15时答案等待时间 最终等待时间公式为 waittime = int(waittime * min(2, (num / 15)))
 coor_switch = True #坐标显示开关
-guess_flag = [] #记忆群内是否猜出答案 是则为{'群号':1}，在为1时不会响应回答语句
-now_playing = [] #防止多局游戏 在为1时不会相应创建游戏指令
-daan = [] #记忆每个群的答案 答案格式为str([列][空格][横])（我也不知道为啥反了，懒得改了） 在一个群没有答案的时候会被置为'error'
+
+game_info = {} #用于记录当前游戏状态，'now_playing'为是否正在游戏中，1为正在游戏，0为并未游戏
+               #'daan'用于记录当前游戏的答案，记录格式为str('[列][空格][行]')，无答案时会被记录为error
+               #'guess_flag'用于记录是否有人猜出答案，是为1，否为0，为1时触发'答案是'语句后会直接return，不做处理
 
 def get_random_position_tkk(num):
     col = random.randint(1, num)
@@ -31,77 +32,52 @@ def get_random_position_tkk(num):
     return col, row
 
 def set_daan(gid, da):
-    global daan
-    for i in daan: 
-        if str(gid) in i.keys():
-            i[str(gid)] = da
-            return
-    else:
-        daan.append({str(gid):da})
-        return
+    global game_info #设置答案时该gid一定存在，不需要不存在处理，不需要返回
+    game_info[gid]['daan'] = da
 
 def cheak_daan(gid, da):
-    for i in daan:
-        if str(gid) in i.keys():
-            if i[str(gid)] == da:
-                return 1
-            else:
-                return 0
+    global game_info
+    if gid not in game_info: #一时半会想不清楚会不会存在检查答案时该群不存在这种情况，反正先处理下
+        game_info[gid] = {}
+        game_info[gid]['now_playing'] = 0
+        game_info[gid]['daan'] = 'error'
+        game_info[gid]['guess_flag'] = 1
+        return 0 #不存在该群，答案肯定不正确
+    elif game_info[gid]['daan'] == da: #回答正确
+        return 1 
     else:
-        daan.append({str(gid):"error"}) #主要防止没有添加答案就来查答案导致报错
-        return 0
-
+        return 0 #回答错误
 
 def close_game(gid):
-    print("now close")
-    global now_playing #在now_playing中一定有这个gid
-    for i in now_playing: #结束游戏
-        if str(gid) in i.keys():
-            i[str(gid)] = 0
-            break
-    global guess_flag
-    for i in guess_flag: 
-        if str(gid) in i.keys(): #游戏关闭时直接设置已猜出
-            i[str(gid)] = 1
-            return
-    else:
-        guess_flag.append({str(gid):1})
-        return
+    global game_info #关闭游戏时该gid一定存在，不需要不存在处理，不需要返回
+    game_info[gid]['now_playing'] = 0
+    game_info[gid]['guess_flag'] = 1
         
-def reset_flag(gid):
-    global guess_flag
-    for i in guess_flag:
-        if str(gid) in i.keys():
-            i[str(gid)] = 0
-            return
-    else:
-        guess_flag.append({str(gid):0})
-        return
-
 def cheak_guess_flag(gid):  
-    global guess_flag
-    for i in guess_flag: 
-        if str(gid) in i.keys():
-            if i[str(gid)] == 1:
-                return 1
-            else:
-                return 0
-    else:
-        return 0
+    global game_info
+    if gid not in game_info: #没有这个gid说明是意外触发
+        game_info[gid] = {}
+        game_info[gid]['now_playing'] = 0
+        game_info[gid]['daan'] = 'error'
+        game_info[gid]['guess_flag'] = 1  #置1直接跳过回复程序防止报错
+        return 1
+    else: #else说明gid存在，直接返回对应值即可
+        return game_info[gid]['guess_flag']
 
 def cheak_now_palying(gid): #检查时顺便设置正在游戏
-    global now_playing
-    for i in now_playing:
-        if str(gid) in i.keys():
-            if i[str(gid)] == 1:
-                return 1
-            else:
-                i[str(gid)] = 1
-                reset_flag(gid)
-                return 0
-    else:
-        now_playing.append({str(gid):1})
-        return 0
+    global game_info
+    if gid not in game_info:
+        game_info[gid] = {}
+        game_info[gid]['now_playing'] = 1
+        game_info[gid]['daan'] = 'error'
+        game_info[gid]['guess_flag'] = 0
+        return 0 #设置为正在游戏中了但是实际并未开始游戏，返回0
+    elif game_info[gid]['now_playing'] == 0:
+        game_info[gid]['now_playing'] = 1
+        game_info[gid]['guess_flag'] = 0 #开始游戏同时重置标志
+        return 0 #同理
+    else: #到这个else说明gid存在且已在游戏中，其实可以直接return 1
+        return game_info[gid]['now_playing']
 
 @sv.on_prefix(('随机唐可可'))                              
 async def random_tkk(bot, ev):  
